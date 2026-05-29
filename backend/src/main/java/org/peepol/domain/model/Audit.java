dc3f61c775b7ed3aa2861183a3a3bd711bc20517
Jelvin Ferreras <jelviferreras01@gmail.com>
2026-05-29 17:17:03 -0400

    Update `Audit` model to permit `Wish` entity

--- backend/src/main/java/org/peepol/domain/model/Audit.java
+++ backend/src/main/java/org/peepol/domain/model/Audit.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
-public abstract sealed class Audit permits Person, User {
+public abstract sealed class Audit permits Person, User, Wish {
 
     @Enumerated
     @Column(nullable = false)
